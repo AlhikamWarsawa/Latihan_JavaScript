4f6adfb44190494af139b684036f7c7026c53f05
AlhikamWarsawa <alhikam200@gmail.com>
2025-03-11 04:51:57 +0700

    Update

--- Bank.js
+++ Bank.js
@@ -13,46 +13,49 @@ if (balance >= 0) {
         alert("Pengguna Tidak Ditemukan")
     } else {
 // Menu Utama
-        let menu = parseInt(prompt(`Menu Utama
+        while (true) {
+            let menu = parseInt(prompt(`Menu Utama
 1. Cek Saldo
 2. Setor Uang
 3. Tarik Uang
 4. Logout`))
-        let setor, tarik, logout
-        switch (menu) {
-            case 1:
-                alert(`Saldo Anda Adalah ${balance}`)
-                break;
-            case 2:
-                setor = parseInt(prompt("Setor Uang Anda"))
-                setor = setor <= 0 ? alert("Angka Tidak Boleh Negatif") : setor
-                if (setor >= 0) {
-                    balance += setor
-                    alert(`Saldo Anda Sekarang Adalah ${balance}`)
-                }
-                break;
-            case 3:
-                tarik = confirm(`Saldo Anda Adalah ${balance} Apakah Anda Ingin Tarik Uang?`)
-                if (tarik) {
-                    tarik = parseInt(prompt("Tarik Uang Anda"))
-                    tarik = tarik <= 0 ? alert("Angka Tidak Boleh Negatif") : tarik >= balance ? alert("Saldo Anda Tidak Cukup") : tarik
-                    if (tarik >= 0 && tarik <= balance) {
-                        balance -= tarik
+            let setor, tarik, logout
+            switch (menu) {
+                case 1:
+                    alert(`Saldo Anda Adalah ${balance}`)
+                    break;
+                case 2:
+                    setor = parseInt(prompt("Setor Uang Anda"))
+                    setor = setor <= 0 ? alert("Angka Tidak Boleh Negatif") : setor
+                    if (setor >= 0) {
+                        balance += setor
                         alert(`Saldo Anda Sekarang Adalah ${balance}`)
                     }
-                }
-                break;
-            case 4:
-                logout = confirm(`Apakah Anda Ingin Logout?`)
-                if (logout) {
-                    alert("Anda Telah Logout")
-                    name = null
-                    balance = null
-                }
-                break;
+                    break;
+                case 3:
+                    tarik = confirm(`Saldo Anda Adalah ${balance} Apakah Anda Ingin Tarik Uang?`)
+                    if (tarik) {
+                        tarik = parseInt(prompt("Tarik Uang Anda"))
+                        tarik = tarik <= 0 ? alert("Angka Tidak Boleh Negatif") : tarik >= balance ? alert("Saldo Anda Tidak Cukup") : tarik
+                        if (tarik >= 0 && tarik <= balance) {
+                            balance -= tarik
+                            alert(`Saldo Anda Sekarang Adalah ${balance}`)
+                        }
+                    }
+                    break;
+                case 4:
+                    logout = confirm(`Apakah Anda Ingin Logout?`)
+                    if (logout) {
+                        alert("Anda Telah Logout")
+                    }
+                    break;
                 default:
                     alert("Menu Tidak Ditemukan")
                     break;
+            }
+            if (menu === 4) {
+                break;
+            }
         }
     }
 }
